194b8c603aabe9dbb42359fb4e493f3c106dfc89
M Arif Nuriman <arifnuriman9@gmail.com>
2022-08-22 13:18:13 +0700

    update sistem loncat
    baru ada movement vertical aja

--- js/player.js
+++ js/player.js
@@ -8,27 +8,35 @@ export class Player {
         this.x = (this.game.width - this.spriteWidth)/2;
         this.y = this.game.height - this.spriteHeight;
         this.image = document.getElementById('char');
-        this.velocity = 0;
-        this.maxvelocity = this.game.height - this.spriteHeight;
+        this.power = 0; // power to jump
+        this.strength = 5; // how much power increase per frame
+        this.maxpower = 50; // maximum power can be
+        this.vy = 0; // horizontal velocity of character
+        this.weight = 1.5; // gravity acceleration = 1, gravity force = 1 * weight
     }
     update(press){
-        if(press){
-            // this.velocity = Math.min(this.maxvelocity, this.velocity+5);
-            this.velocity = 100;
-        } else {
-            this.y -= this.velocity;
-            this.velocity-=1;
-    
-            if(this.y > this.game.width - this.spriteHeight){
-                // console.log(true);
-                this.y = this.game.height - this.spriteHeight;
-                this.velocity = 0;
+
+        // Horizontal movement
+        if(this.onGround()){
+            if(press){
+                this.power = Math.min(this.maxpower, this.power+this.strength); // charge up power to jump
+            } else {
+                this.vy = -this.power; // changing power into horizontal velocity (upward velocity is minus)
+                this.power = 0; // reset charged power
             }
         }
-        // console.log(this.velocity);
+        this.y += this.vy; // change position based on velocity
+        if(!this.onGround()){
+            this.vy += this.weight; // vt = vo + gt
+        } else {
+            this.vy = 0; // when hit the ground, horizontal velocity is 0
+        }
+        if(this.y > this.game.height - this.height) this.y = this.game.height - this.height // corrector if fps is not good
     }
     draw(context){
-        // context.fillRect(this.x, this.y, this.width, this.height);
         context.drawImage(this.image, 0*this.spriteWidth, 0*this.spriteHeight, this.spriteWidth, this.spriteHeight,  this.x, this.y, this.width, this.height);
+    }
+    onGround(){
+        return this.y >= this.game.height - this.height
     }
 }
